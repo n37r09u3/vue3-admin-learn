a21680505c32158ebd4bb41fa2c64f4f4870e835
n37r09u3 <n37r09u3@gmail.com>
2022-02-02 15:22:33 +0800

    2-6 结束

---
+++ src/config/index.js
@@ -0,0 +1,23 @@
+/**
+ * 环境配置封装
+ */
+const env = import.meta.env.MODE || 'prod';
+const EnvConfig = {
+    development:{
+        baseApi:'/api',
+        mockApi:'https://www.fastmock.site/mock/5707fc1b97008f67471c7f4f3b43eb81/api'
+    },
+    test:{
+        baseApi:'//test.futurefe.com/api',
+        mockApi:'https://www.fastmock.site/mock/c1c302e8baed9894c48c17e4738c092e/api'
+    },
+    prod:{
+        baseApi:'//futurefe.com/api',
+    }
+}
+export default {
+    env,
+    mock:false,
+    namespace:'manager',
+    ...EnvConfig[env]
+}
--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import router from "./router";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
+import axios from 'axios'
 const app = createApp(App)
+console.log('env=>',import.meta.env)
+import config from './config'
+axios.post(config.mockApi+'/login').then(function(response) {
+    console.log(response)
+})
+
 app.use(router)
 app.use(ElementPlus)
 app.mount('#app')
+
