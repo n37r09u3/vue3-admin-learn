e95dc83164192f47b03074b4d5b7e28f2f2682f8
n37r09u3 <n37r09u3@gmail.com>
2022-02-04 00:45:56 +0800

    5-2

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,9 +1,45 @@
 <template>
   <div class="basic-layout">
-    <div class="nav-side">1</div>
+    <div class="nav-side">
+      <div class="logo">
+        <img src="../assets/images/logo.png" alt="">
+        <span>vue3-admin</span>
+      </div>
+      <el-menu
+          default-active="1-1"
+          class="nav-menu"
+          router
+          background-color="#001529"
+          text-color="#fff"
+          :collapse="false"
+      >
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon><setting /></el-icon>
+            <span>系统管理</span>
+          </template>
+            <el-menu-item index="1-1">用户管理</el-menu-item>
+            <el-menu-item index="1-2">菜单管理</el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="2">
+          <template #title>
+            <el-icon><setting /></el-icon>
+            <span>审批管理</span>
+          </template>
+          <el-menu-item index="2-1">休假管理</el-menu-item>
+          <el-menu-item index="2-2">待我审批</el-menu-item>
+        </el-sub-menu>
+
+      </el-menu>
+    </div>
     <div class="content-right">
+
       <div class="nav-top">
-        <div class="bread">面包屑</div>
+        <div class="nav-left">
+          <div class="menu-fold"> <el-icon><fold /></el-icon></div>
+          <div class="bread">面包屑</div>
+        </div>
+
         <div class="user-info">{{userauth.userInfo.userName}}</div>
       </div>
       <div class="wrapper">
@@ -32,6 +68,21 @@ const userauth = useUserAuthStore()
     color: white;
     overflow-y: auto;
     transition: width 0.5s;
+    .logo{
+      display:flex;
+      align-items: center;
+      font-size: 18px;
+      height:50px;
+      img{
+        margin: 0 15px;
+        width:32px;
+        height:32px;
+      }
+    }
+    .nav-menu{
+      border-right: none;
+      //height:calc(100vh - 50px);
+    }
   }
   .content-right{
     margin-left: 200px;
@@ -43,6 +94,7 @@ const userauth = useUserAuthStore()
       justify-content: space-between;
       border-bottom: 1px solid #ddd;
       padding: 0 20px;
+
     }
     .wrapper{
       background: #eef0f3;
